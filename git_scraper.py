3c0625eb5b583e8a131fe561fdccac83e17e4edd
monu <mdehsan873@gmail.com>
2022-01-22 09:56:41 +0530

    After fixes

--- git_scraper.py
+++ git_scraper.py
@@ -3,28 +3,29 @@ import urllib
 import csv
 import constant
 import time
-def get_user_details(user):
 
-    req=urllib.request.Request(constant.USER_URL+user, headers={'User-Agent' : "Magic Browser"})
-    page = urllib.request.urlopen( req )
-    soup=BeautifulSoup(page.read(),"html.parser")
+
+def get_user_details(user):
+    req = urllib.request.Request(constant.USER_URL + user, headers={'User-Agent': "Magic Browser"})
+    page = urllib.request.urlopen(req)
+    soup = BeautifulSoup(page.read(), "html.parser")
 
     save_details = {}
-    repositories=soup.find('nav',{'class','UnderlineNav-body width-full p-responsive'}).span.text
-    followers=soup.find('div',{'class','flex-order-1 flex-md-order-none mt-2 mt-md-0'}).span.text
-    save_details['FOLLOWERS']=followers
-    save_details['REPOSTRIES']=repositories
+    repositories = soup.find('nav', {'class', 'UnderlineNav-body width-full p-responsive'}).span.text
+    followers = soup.find('div', {'class', 'flex-order-1 flex-md-order-none mt-2 mt-md-0'}).span.text
+    save_details['FOLLOWERS'] = followers
+    save_details['REPOSTRIES'] = repositories
     return save_details
 
-def get_details(user):
 
-    user_name = user.find('a',{'class','color-fg-muted'})
+def get_details(user):
+    user_name = user.find('a', {'class', 'color-fg-muted'})
     try:
-        user_name=user_name.em.string
-    except:
+        user_name = user_name.em.string
+    except Exception:
         return
-    name = user.find('div',{'class','f4 text-normal'}).a.text.strip()
-    bio = user.find('p',class_='mb-1')
+    name = user.find('div', {'class', 'f4 text-normal'}).a.text.strip()
+    bio = user.find('p', class_='mb-1')
 
     save_details = {}
     save_details['NAME'] = name
@@ -34,36 +35,36 @@ def get_details(user):
     else:
         save_details['BIO'] = constant.BIO
     time.sleep(5)
-    save_details.update (get_user_details(user_name))
+    save_details.update(get_user_details(user_name))
     print(save_details)
     return save_details
 
+
 def scraper(name):
-    fields = ['NAME', 'USER_NAME', 'BIO','FOLLOWERS','REPOSTRIES']
-    out_file = open('scraped_data.csv','w')
-    csvwriter = csv.DictWriter(out_file, delimiter=',', fieldnames=fields) 
-    save_data={}
-    page_no=0
+    fields = ['NAME', 'USER_NAME', 'BIO', 'FOLLOWERS', 'REPOSTRIES']
+    out_file = open('scraped_data.csv', 'w')
+    csvwriter = csv.DictWriter(out_file, delimiter=',', fieldnames=fields)
+    save_data = {}
+    page_no = 0
     while True:
 
-        if page_no>10:
+        if page_no > 10:
             break
 
-        url=constant.BASE_URL+str(page_no)+constant.ATTRIBUTE1+name+constant.SEARCH_TYPE
-        req=urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-        page = urllib.request.urlopen( req )
-        soup=BeautifulSoup(page.read(),"html.parser")
-        
+        url = constant.BASE_URL + str(page_no) + constant.ATTRIBUTE1 + name + constant.SEARCH_TYPE
+        req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"})
+        page = urllib.request.urlopen(req)
+        soup = BeautifulSoup(page.read(), "html.parser")
+        users = soup.find_all('div', {'class', 'd-flex hx_hit-user px-0 Box-row'})
         try:
-            users=soup.find_all('div',{'class','d-flex hx_hit-user px-0 Box-row'})
             for user in users:
-                save_data=(get_details(user))
+                save_data = (get_details(user))
                 if save_data:
                     csvwriter.writerow(save_data)
-            page_no+=1   
+            page_no += 1
             time.sleep(5)
-        
-        except:
+
+        except Exception:
             print(constant.ERROR)
             break
 
--- launcher.py
+++ launcher.py
@@ -1,7 +1,8 @@
 import git_scraper as git
-name=input("Enter name you want to  get data\n")
+
+name = input("Enter name you want to  get data\n")
 if name:
-    name=name.replace(' ','+')
+    name = name.replace(' ', '+')
     git.scraper(name)
 else:
     print("Enter Valid Input")
