45413bd252948eef910ced769a7a00c4d46ddb3f
monu <mdehsan873@gmail.com>
2022-01-22 14:09:54 +0530

    Changes made after review

--- launcher.py
+++ launcher.py
@@ -1,8 +1,9 @@
 import git_scraper
 
 name = input("Enter Query\n")
-if name:
-    name = name.replace(' ', '+')
-    git_scraper.scraper(name)
-else:
-    print("Enter Valid Input")
+while True:
+    if name:
+        name = name.replace(' ', '+')
+        git_scraper.scraper(name)
+    else:
+        print("Enter Valid Input")
